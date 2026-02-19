7e0eb0d1c0973da269671c7d1e798e5d1c59f90f
AndresSaavedra01 <andres.saavedra01@uceva.edu.co>
2026-02-19 08:17:50 -0500

    se ajustan cosas

--- src/main/java/game/gui/Window.java
+++ src/main/java/game/gui/Window.java
@@ -1,19 +1,22 @@
 package game.gui;
 
-import game.entities.Aspects;
+import game.entities.*;
+import game.entities.Character;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Window extends JFrame {
 
 
-    private final int width = 1080;
-    private final int height =  720;
-
-
     public Window() {
         super("Rpgero");
+
+        int width = 1080;
+        int height = 720;
 
         Toolkit tools = Toolkit.getDefaultToolkit();
         Image icon = tools.getImage("src/images/icons8-pepe-the-frog-48.png");
@@ -25,58 +28,94 @@ public class Window extends JFrame {
         add(new CharactersContainer(), BorderLayout.CENTER);
         add(new SkillPanel(), BorderLayout.SOUTH);
 
+        List<Character> allies = createAllies();
+        assert allies != null;
+        allies.add(createPlayer());
+        BattleContext battleContext = new BattleContext(allies,createEnemies());
+        Battle battle = new Battle(battleContext);
 
         setVisible(true);
 
     }
 
-    class SkillPanel extends JPanel{
-        public SkillPanel(){
-            setLayout(new FlowLayout(FlowLayout.CENTER,100,20));
-            setBackground(Color.BLUE);
-            add(new JButton("otorinolaringologo"));
-            add(new JButton("PEPE"));
-            add(new JButton("PEPE"));
-            add(new JButton("PEPE"));
-        }
+
+    private Character createPlayer(){
+        return null;
     }
 
-    class CharactersContainer extends JPanel{
-        public CharactersContainer() {
-            setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-            setBackground(Color.PINK);
-            JPanel p = new JPanel();
-            JPanel p1 = new JPanel();
-            p.setBackground(Color.GREEN);
-            p1.setBackground(Color.GRAY);
+    private List<Character> createAllies(){
 
-
-            add(new AspectCharacterPanel(Aspects.getImages().get(2)));
-            add(new AspectCharacterPanel(Aspects.getImages().get(3)));
-        }
+        return null;
     }
 
-    class AspectCharacterPanel extends JPanel{
-        Image aspect;
-        static boolean pepe = true;
-        public AspectCharacterPanel(Image aspect) {
-            this.aspect =  aspect;
-            if (pepe)setBackground(Color.BLUE);
-            if (!pepe)setBackground(Color.GRAY);
-            pepe=!pepe;
-            scaledImage();
+    private List<Character> createEnemies(){
+        return null;
+    }
 
-        }
+}
 
-        private void scaledImage(){
-            aspect = aspect.getScaledInstance(aspect.getWidth(this)*500,aspect.getWidth(this)*500,Image.SCALE_DEFAULT);
-        }
+class SkillPanel extends JPanel{
+    public SkillPanel(){
+        setLayout(new FlowLayout(FlowLayout.CENTER,100,20));
+        setBackground(Color.BLUE);
+        add(new SkillButton("otorinolaringologo"));
+        add(new SkillButton("PEPE"));
+        add(new SkillButton("PEPE"));
+        add(new SkillButton("PEPE"));
+    }
+}
 
-        @Override
-        public void paint(Graphics g) {
-            super.paint(g);
-            g.drawImage(aspect, this.getWidth()/2-(aspect.getWidth(this)/2),this.getHeight()/2-(aspect.getHeight(this)/2), this);
-        }
+class CharactersContainer extends JPanel{
+    public CharactersContainer() {
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        setBackground(Color.PINK);
+        JPanel p = new JPanel();
+        JPanel p1 = new JPanel();
+        p.setBackground(Color.GREEN);
+        p1.setBackground(Color.GRAY);
+
+
+        add(new AspectCharacterPanel(Aspects.getImages().get(2)));
+        add(new AspectCharacterPanel(Aspects.getImages().get(3)));
+    }
+}
+
+class AspectCharacterPanel extends JPanel{
+    Image aspect;
+    static boolean pepe = true;
+
+    public AspectCharacterPanel(Image aspect) {
+        this.aspect =  aspect;
+        if (pepe)setBackground(Color.BLUE);
+        if (!pepe)setBackground(Color.GRAY);
+        pepe=!pepe;
+        scaledImage();
+
+    }
+
+    private void scaledImage(){
+        aspect = aspect.getScaledInstance(500,500,Image.SCALE_DEFAULT);
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(aspect, this.getWidth()/2-(aspect.getWidth(this)/2),this.getHeight()/2-(aspect.getHeight(this)/2), this);
+    }
+}
+
+class SkillButton extends JButton{
+    public SkillButton(String text) {
+        super(text);
+        setBackground(Color.DARK_GRAY);
+        setForeground(Color.WHITE);
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
 }
